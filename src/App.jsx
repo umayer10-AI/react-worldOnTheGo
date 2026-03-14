815342074099a377e7c955fcc43f061d6f6e0c06
umayer10-AI <mdumayer10@gmail.com>
2026-03-14 18:58:48 +0600

    add

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Countries from './component/countries/Countries'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello react</h1>
+      <Countries></Countries>
+    </div>
+  )
+}
+
+export default App
